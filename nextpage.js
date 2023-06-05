13879168b9c2a4c5281cd1db611d1c67e58423ab
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-06-05 12:29:12 +0600

    Update nextpage.js

--- nextpage.js
+++ nextpage.js
@@ -1,30 +1,3 @@
-jQuery(document).ready(function($){
-// автообновление 10сек
-// setTimeout(function(){
-//     location.reload();
-// }, 5000);
-
-
-// btn1.onclick = function() {
-//     // if (this.innerHTML=='ON') this.innerHTML = 'OFF';
-//     // else this.innerHTML = 'ON';
-//     // return false;
-// }
-const text = document.getElementById("text");
-const button = document.getElementById("btn1");
-
-// button.addEventListener("click", function() {
-//   text.textContent = "Новый текстe";
-// });
-button.addEventListener("click", function() {
-  fetch('citatadark.txt')
-    .then(response => response.text())
-    .then(data => {
-      const lines = data.split('\n');
-      const randomIndex = Math.floor(Math.random() * lines.length);
-      text.textContent = lines[randomIndex];
-    });
-});
-
-
-});
+const btn1 = document.getElementById('btn1');
+      btn1.addEventListener('click', function() {
+        window.location.href = 'menu.html';
