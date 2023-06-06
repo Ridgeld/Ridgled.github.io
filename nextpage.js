1acc305c41d1a117bce911a9c34fddeb898a489d
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-06-06 12:08:23 +0600

    Update nextpage.js

--- nextpage.js
+++ nextpage.js
@@ -1,12 +1,13 @@
-const username = document.getElementById('nick').value;
-const password = document.getElementById('pass').value;
 const btn1 = document.getElementById('btn1');
       btn1.addEventListener('click', function() {
-        if (username === '1') {
+          var username = document.getElementById('nick').value;
+          var password = document.getElementById('pass').value
+
+        if(username === 'admin' && password === '12345') {
+                window.location.href = 'database.html';
+        }else if(username ==='1') {
           window.location.href = 'menu.html';
-        } else if (username === 'admin' && password === '12345') {
-          window.location.href = 'database.html';
-        } else {
-          alert('Неверное имя пользователя или пароль');
+        }else{
+            alert('Неверное имя пользователя или пароль');
         }
 });
