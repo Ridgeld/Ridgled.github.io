9849ec6461144f1189f77a5fe731a3e35dcf1864
Daniel <rayl.alim.2014@gmail.com>
2023-11-06 12:23:52 +0600

    corrections

--- js/task_item.js
+++ js/task_item.js
@@ -17,7 +17,7 @@ function addItems(){
             task_body.classList.add(class_name.class);
             const task_pattern = document.createElement('div');
             task_pattern.className = "task_pattern";
-            task_pattern.classList.add(`pattern_${class_name.pattern}`)
+            task_pattern.classList.add(`pattern_${class_name.pattern}`);
 
             task_body.addEventListener("click", () => {
                 // Переадресация на task.html с передачей параметра "lessonName"
